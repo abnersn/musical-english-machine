db41abc8fc8469d3e29fb15e5a3255c11796e95f
Abner <contact@abnersn.dev>
2024-11-03 22:13:41 -0300

    Basic spotify redirect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 function App() {
-  const handleClickAuthenticate = () => {
-    console.log('auth');
-  };
   return (
     <main>
       <h1>Musical English Machine</h1>
       <section>
         <h2>Spotify</h2>
-        <button onClick={handleClickAuthenticate}>
-          Click here to authenticate
-        </button>
+        <p>
+          Para começar, precisamos da sua autorização para acessar as músicas
+          que você tem ouvido no Spotify.
+        </p>
+        <form action="/api/authorize" method="get">
+          <button type="submit">Clique aqui para autorizar</button>
+        </form>
       </section>
     </main>
   );
--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,7 +1,24 @@
+from os import environ
+
 from flask import Flask
+from flask import redirect
+from urllib.parse import urlencode
+import hashlib, secrets
 
 app = Flask(__name__)
 
 @app.route("/api")
 def main():
   return "<p>Hello, world!</p>"
+
+@app.route("/api/authorize")
+def authorize():
+  state = hashlib.sha256(secrets.token_bytes(16)).hexdigest()
+  data = urlencode({
+    "response_type": 'code',
+    "client_id": environ.get("SPOTIFY_CLIENT_ID"),
+    "scope": "user-read-recently-played",
+    "redirect_uri": "http://localhost",
+    "state": state
+  })
+  return redirect(f'https://accounts.spotify.com/authorize?{data}')
