e3aac40e6d2a9dae511ee3dc6994d46c4993febf
Abner <contact@abnersn.dev>
2024-11-03 21:08:23 -0300

    Basic code for server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello, react!</h1>;
+  const handleClickAuthenticate = () => {
+    console.log('auth');
+  };
+  return (
+    <main>
+      <h1>Musical English Machine</h1>
+      <section>
+        <h2>Spotify</h2>
+        <button onClick={handleClickAuthenticate}>
+          Click here to authenticate
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
